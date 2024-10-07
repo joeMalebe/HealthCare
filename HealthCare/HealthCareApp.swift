d88807366b48f94485037c1fab7e601cb4ad3e62
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-10-07 20:24:45 +0200

    Initial Commit

---
+++ HealthCare/HealthCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCareApp.swift
+//  HealthCare
+//
+//  Created by Malebe, Joe J on 2024/10/07.
+//
+
+import SwiftUI
+
+@main
+struct HealthCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
